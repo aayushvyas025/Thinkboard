43b17e99a74a76838a009e8e318c4568ca307bf6
Aayush Vyas <aayush25vyas@gmail.com>
2026-07-24 19:01:44 +0530

    not-found page

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,29 @@
+import { FileQuestionIcon } from "lucide-react";
+import { Link } from "react-router";
+
+function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center py-16 space-y-6 w-full max-w-xl mx-auto text-center">
+      <div className="bg-error/10 rounded-full p-8">
+        <FileQuestionIcon className="size-10 text-error" />
+      </div>
+
+      <h1 className="text-5xl font-extrabold text-error">404</h1>
+
+      <h2 className="text-2xl font-bold">
+        Oops! Page Not Found
+      </h2>
+
+      <p className="text-base-content/70">
+        The page you're looking for doesn't exist, has been moved,
+        or the URL may be incorrect.
+      </p>
+
+      <Link to="/" className="btn btn-primary">
+        Back to Home
+      </Link>
+    </div>
+  );
+}
+
+export default NotFound;
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router";
 const CreatePage = lazy(() => import("../pages/Create"));
 const HomePage = lazy(() => import("../pages/Home"));
 const NoteDetailPage = lazy(() => import("../pages/NoteDetail"));
+const NotFoundPage = lazy(() => import("../pages/NotFound"));
 
 function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ function AppRoutes() {
       <Route path="/" element={<HomePage />} />
       <Route path="/create" element={<CreatePage />} />
       <Route path="/detail/:id" element={<NoteDetailPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
